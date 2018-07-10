499895482a9ba12cebc28106b3fa2587242c8544
Aditya Gothwal <adityagothwal13@gmail.com>
2018-07-10 23:02:51 +0530

    Add files via upload
    Prints the shape of the image, produces colored image and it's black and white replica.

--- imagecv.py
+++ imagecv.py
@@ -1,15 +1,16 @@
 import cv2
 
 #loading image
-img = cv2.imread('one.jpeg',0)
-img1 = cv2.imread('three.jpeg')
+img = cv2.imread('image.jpg',0)
+img1 = cv2.imread('image.jpg')
 
 #Printing height and width
 print (img.shape)
 
-#to display that image
-cv2.imshow("Nature", img)
-#cv2.imshow("Naturenew", img1)
+#to display the B&W image
+cv2.imshow("Nature_B&W", img)
+#to display the colored one
+cv2.imshow("Nature", img1)
 
 #image window holder activation
 cv2.waitKey(0)
